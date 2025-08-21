9b3c863a8bb5dbc4a55167d47ce6036a60ab6201
German-Shein-1995 <germanshein1995@gmail.com>
2025-08-22 02:43:18 +0400

    [Issue #1799](https://github.com/readest/readest/issues/1799): Changing the name order when sorting the book by Author by placing the last name first in Arabic, Tibetan, German, English, Spanish, French, Hindi, Italian, Dutch, Polish, Portuguese, Russian, Thai, Turkish and Ukrainian languages

--- apps/readest-app/src/app/library/components/Bookshelf.tsx
+++ apps/readest-app/src/app/library/components/Bookshelf.tsx
@@ -239,8 +239,8 @@ const Bookshelf: React.FC<BookshelfProps> = ({
         const bTitle = formatTitle(b.title);
         return aTitle.localeCompare(bTitle, uiLanguage || navigator.language);
       case 'author':
-        const aAuthors = formatAuthors(a.author);
-        const bAuthors = formatAuthors(b.author);
+        const aAuthors = formatAuthors(['ar', 'bo', 'de', 'en', 'es', 'fr', 'hi', 'it', 'nl', 'pl', 'pt', 'ru', 'th', 'tr', 'uk'].includes (a?.primaryLanguage || 'en') ? `${a.author.split(' ').at(-1)} ${a.author.split(' ').slice(0, -1).join(' ')}` : a.author);
+        const bAuthors = formatAuthors(['ar', 'bo', 'de', 'en', 'es', 'fr', 'hi', 'it', 'nl', 'pl', 'pt', 'ru', 'th', 'tr', 'uk'].includes (b?.primaryLanguage || 'en') ? `${b.author.split(' ').at(-1)} ${b.author.split(' ').slice(0, -1).join(' ')}` : b.author);
         return aAuthors.localeCompare(bAuthors, uiLanguage || navigator.language);
       case 'updated':
         return new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime();
